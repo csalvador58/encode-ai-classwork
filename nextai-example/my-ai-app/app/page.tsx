e7291dfd27d0223d45ee2bb185c44f02f529def3
Chris Salvador <csalvador58@gmail.com>
2024-03-26 11:16:04 -0700

    updated for image generation

--- nextai-example/my-ai-app/app/page.tsx
+++ nextai-example/my-ai-app/app/page.tsx
@@ -1,17 +1,12 @@
 'use client';
 
 import { useChat } from 'ai/react';
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 export default function Chat() {
-  const {
-    append,
-    isLoading,
-    messages,
-    input,
-    handleInputChange,
-    handleSubmit,
-  } = useChat();
+  const { append, isLoading, messages } = useChat();
+  const [imageIsLoading, setImageIsLoading] = useState(false);
+  const [image, setImage] = useState<string | null>(null);
   const messagesContainerRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
@@ -20,6 +15,31 @@ export default function Chat() {
         messagesContainerRef.current.scrollHeight;
     }
   }, [messages]);
+
+  if (imageIsLoading) {
+    return (
+      <div className='flex justify-center items-center h-screen'>
+        <div className='loader'>
+          <div className='animate-pulse flex space-x-4'>
+            <div className='rounded-full bg-slate-700 h-10 w-10'></div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  if (image) {
+    return (
+      <div className='card w-full h-screen max-w-md py-24 mx-auto stretch'>
+        <img src={`data:image/jpeg;base64,${image}`} />
+        <textarea
+          className='mt-4 w-full text-white bg-black h-64'
+          value={messages[messages.length - 1].content}
+          readOnly
+        />
+      </div>
+    );
+  }
   return (
     <div className='flex flex-col w-full h-screen max-w-md py-24 mx-auto stretch'>
       <div className='overflow-auto w-full mb-8' ref={messagesContainerRef}>
@@ -45,25 +65,49 @@ export default function Chat() {
 
       <div className='fixed bottom-0 mt-4 w-full max-w-md'>
         <div className='flex flex-col justify-center mb-6 items-center'>
-          <button
-            className='bg-blue-500 p-2 text-white rounded shadow-xl'
-            disabled={isLoading}
-            onClick={() =>
-              append({ role: 'user', content: 'Give me a random recipe' })
-            }
-          >
-            Random Recipe
-          </button>
-          <p className='p-4'>OR</p>
+          {messages.length == 0 && (
+            <button
+              className='bg-blue-500 p-2 text-white rounded shadow-xl'
+              disabled={isLoading}
+              onClick={() =>
+                append({ role: 'user', content: 'Give me a random recipe' })
+              }
+            >
+              Random Recipe
+            </button>
+          )}
+          {messages.length == 2 && !isLoading && (
+            <button
+              className='bg-blue-500 p-2 text-white rounded shadow-xl'
+              disabled={isLoading}
+              onClick={async () => {
+                setImageIsLoading(true);
+                const response = await fetch('api/images', {
+                  method: 'POST',
+                  headers: {
+                    'Content-Type': 'application/json',
+                  },
+                  body: JSON.stringify({
+                    message: messages[messages.length - 1].content,
+                  }),
+                });
+                const data = await response.json();
+                setImage(data);
+                setImageIsLoading(false);
+              }}
+            >
+              Generate image
+            </button>
+          )}
         </div>
-        <form onSubmit={handleSubmit}>
+        {/* <form onSubmit={handleSubmit}>
           <input
             className='fixed bottom-0 w-full max-w-md p-2 mb-8 border border-gray-300 rounded shadow-xl text-black'
             value={input}
             placeholder='Say something...'
             onChange={handleInputChange}
           />
-        </form>
+        </form> */}
       </div>
     </div>
   );
